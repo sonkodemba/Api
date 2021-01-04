fbef1485c75e3c3aa38c07e118998d05d528ea4e
sonkodemba <sonkodemba@gmail.com>
2021-01-04 05:06:02 +0000

    initial commit

---
+++ app/Http/Controllers/dummyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class dummyController extends Controller
+{
+    //
+    public function getData()
+    {
+        return "data is returned from the getData()";
+    }
+}
